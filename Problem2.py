502c9c545c2e8750505a822892959fafa6d2051b
Mudita Bysani <muditabysani@Muditas-MacBook-Pro.local>
2019-08-24 15:35:47 -0700

    Completed Competitive-Coding-4

---
+++ Problem2.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        # base case
+        if root == None:
+            return True
+        # finding the height of the left subtree and right subtree
+        left_height = self.helper(root.left)
+        right_height = self.helper(root.right)
+        if abs(left_height - right_height) > 1:
+            # the height difference between the left and right subtree must be less than or equal to 1
+            return False
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def helper(self, root):
+        # base case
+        if root == None:
+            return 0
+        left = self.helper(root.left) + 1
+        right = self.helper(root.right) + 1
+        return max(left, right)
