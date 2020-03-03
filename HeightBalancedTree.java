f8460accf2a950ba92863cd83058bcdd5826295e
SomiDeev <sowmyadvn.92@gmail.com>
2020-03-02 21:21:13 -0800

    CC-4 done

--- HeightBalancedTree.java
+++ HeightBalancedTree.java
@@ -3,19 +3,29 @@ Time complexity: O(N)
 Space complexity: O(H)
 Algorithm:
 =========
-1. 
+1. Calculate heights of left and right subtrees and check difference of heights of both at every step
+2. If at any point of time, if difference of heights is greater than 1, return false
+3. Else, tree is height-balanced
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root == null) return true;
-        
-        int height = Math.abs(height(root.left) - height(root.right));
-        return height <= 1 && isBalanced(root.left) && isBalanced(root.right);
+       if(root == null)
+           return true;
+        return height(root)!= -1 ;			
     }
     
     private int height(TreeNode root) {
-        if(root == null) return -1;
+        if(root == null)
+            return 0;						// edge case
         
-        return Math.max(height(root.left), height(root.right))+1;
+        int left = height(root.left);		// check if left subtree is height-balanced recursively
+        if(left == -1)
+            return -1;
+        int right = height(root.right);		// check if right subtree is height-balanced recursively
+        if(right == -1)
+            return -1;
+        if(Math.abs(left-right) > 1)		// if difference of heights of both subtrees greater than 1, not height balanced
+            return -1;
+        return Math.max(left,right)+1;		// Return max height of binary tree if height balanced
     }
 }
