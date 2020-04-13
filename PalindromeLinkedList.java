74aea81bb32f511198ae26126ed5126d82a3570b
RutujaKaushike <rutujakaushike@gmail.com>
2020-04-13 00:26:09 -0500

    updating time and space complexity

--- PalindromeLinkedList.java
+++ PalindromeLinkedList.java
@@ -1,3 +1,8 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
 public class PalindromeLinkedList {
     public boolean isPalindrome(ListNode head) {
 
