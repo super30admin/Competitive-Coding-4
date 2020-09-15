489133232bdce8c346388ff635f52affa3cee130
rajesh-durbha <rajesh.durbha@gmail.com>
2020-09-15 01:28:13 -0500

    Completed Competitive-Coding-4 - Please Review

--- LLPalindrome.java
+++ LLPalindrome.java
@@ -1,10 +1,13 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(3n/2)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
-// Your code here along with comments explaining your approach
+// By Using FLoyd's Algorithm, slow and fast pointers are iterated to find the midpoint of the linked list.
+// Return the fast pointer to head and reverse the linked list from the slow pointer to the end.
+// No compare every node of slow and fast pointers which is iterating in opposite directions.
+// if both values are not equal, return false as it has to be the same if it is palindrome.
 
 /**
  * Definition for singly-linked list.
