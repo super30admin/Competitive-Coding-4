bdd1d7161981ece6dac1ad8b19503163d5463ab8
Lohita Gundapaneni <lohitagundapaneni@Lohitas-MacBook-Air.local>
2021-01-18 20:56:15 -0500

    Compettitve Coding-4 done

---
+++ BalancedBinaryTree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+private boolean result = true;
+
+public boolean isBalanced(TreeNode root) {
+    maxDepth(root);
+    return result;
+}
+
+public int maxDepth(TreeNode root) {
+    if (root == null)
+        return 0;
+    int l = maxDepth(root.left);
+    int r = maxDepth(root.right);
+    if (Math.abs(l - r) > 1)
+        result = false;
+    return 1 + Math.max(l, r);
+}
+}
+
+//Time complexity : O(n) where n is the number of elements in binary tree
+//Space complexity : O(h) where h is the height of binary tree
