84c4f77144a038c626fc4e197a0b0d5f127e2563
amby2964 <75859704+amby2964@users.noreply.github.com>
2021-01-19 19:06:04 -0700

    Update tree-balance.py

--- tree-balance.py
+++ tree-balance.py
@@ -5,20 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    '''
+    Time Complexity: O(n)
+    Space Complexity: O(n) for recursive stack
+    '''
     def isBalanced(self, root: TreeNode) -> bool:
         # bottom up recursion
         if(root==None) or (root.left==None and root.right==None):
             return True
-        res = True
+        #res = True
         def balance(root,depth):
             if(root==None):
-                return depth
-            d_left = balance(root.left,depth+1)
-            d_right = balance(root.right,depth+1)
+                return depth,True
+            d_left,isB = balance(root.left,depth+1)
+            if(not isB):
+                return 0,False
+            d_right,isB = balance(root.right,depth+1)
+            if(not isB):
+                return 0,False
             if(abs(d_left-d_right)>1):
-                nonlocal res 
-                res= False
-            return max(d_left,d_right)
+                return 0, False
+            return max(d_left,d_right),True
         
-        balance(root,0)
-        return res
+        
+        return balance(root,0)[0]
