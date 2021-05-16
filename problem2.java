571b936829908f73502b05fa01f7d05c8fc67af9
SaurabhEdge <ss7586@rit.edu>
2021-05-16 12:10:34 -0400

    Completed Competitive coding 4 sprint

---
+++ problem2.java
@@ -0,0 +1,47 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+
+// Your code here along with comments explaining your approach
+
+
+public class problem2 {
+
+public boolean isPalindrome(ListNode head) {
+        
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        
+        fast=head;
+        slow=reverse(slow);
+        
+        while(slow!=null){
+            if(slow.val!=fast.val){
+                return false;
+            }
+            slow=slow.next;
+            fast=fast.next;
+        }
+        return true;
+             
+    }
+    
+    private ListNode reverse(ListNode head){
+        ListNode prev=null;
+        
+        while(head!=null){
+            ListNode temp=head.next;
+            head.next=prev;
+            prev=head;
+            head=temp;
+        }
+        return prev;
+    }
+    
+}
