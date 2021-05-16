428323d546870c1205e0fe3f073e276fef81f484
prapalak <prapalak@cisco.com>
2021-05-16 15:12:00 -0700

    Implemented LinkedList palindrome.

--- BalancedBinaryTree.java
+++ BalancedBinaryTree.java
@@ -30,16 +30,23 @@ public class BalancedBinaryTree{
     
     public static int helper(TreeNode root){
         // base case
+        // Height of the tree when node is null.
         if(root == null) return 0;
         
         // logic
+        // Calculate the height of the left and the right sub trees.
         int leftHeight = helper(root.left);
         int rightHeight = helper(root.right);
         
+        // check the absolute difference of the heights between the left and the right sub trees.
+        // If the difference in the heights is greater than 1 then the tree is not balanced
         if(Math.abs(leftHeight - rightHeight) > 1) {
             isBalanced = false;
         }
         
+        // Propagating the height of the current node to the parent tree.
+        // Height of the parent node is equal to 1 + Math.max(height of left sub tree, height of right sub tree)
+        // 1 in the above equation is to include the height of the current node.
         return 1 + Math.max(leftHeight, rightHeight);
     }
 
