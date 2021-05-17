b011094df07a2ab9d03faba9b58d1046e3be78b1
Beryl Mario Shairu Joseph Antony Bose <44846387+Beryl2208@users.noreply.github.com>
2021-05-16 23:45:57 -0400

    Completed Comp Coding-4

---
+++ P1.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+//time-o(n)
+//space- o(h)
+
+//passed in leetcode- yes
+
+class Solution {
+    boolean isBalanced;
+    
+    public boolean isBalanced(TreeNode root) {
+        isBalanced=true;
+        int curht= helper(root,0);
+        return isBalanced;
+    }
+    
+    public int helper(TreeNode root, int currht)
+    {
+        if(root==null) return 0;
+        
+        if(root.left== null && root.right==null) return currht+1;
+        
+        int leftht = helper(root.left, currht);
+        int rightht = helper(root.right, currht);
+        
+        if(Math.abs(leftht-rightht) >1)
+        {
+            isBalanced = false;
+        }
+        
+        return Math.max(leftht,rightht)+1;
+        
+    }
+}
