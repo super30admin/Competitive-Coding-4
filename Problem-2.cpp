241e1812e044cd662dfe1902de9ae29e05e72c70
Durgesh Patil <patildurgesh1998@gmail.com>
2021-08-20 20:36:22 -0400

    Update Problem-2.cpp

--- Problem-2.cpp
+++ Problem-2.cpp
@@ -8,6 +8,9 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
+  Time Complexity = O(N*N)
+ Space Complexity = O(h)
+ where N is the number of nodes in the tree and h is the height of the tree.
  */
 class Solution {
 public:
@@ -21,5 +24,42 @@ public:
         if(root==NULL)
             return 0;
         return max(height(root->left),height(root->right))+1;
+    }
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ Time Complexity = O(N)
+ Space Complexity = O(h)
+ where N is the number of nodes in the tree and h is the height of the tree.
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        int height=0;
+        return check(root, &height);
+    }
+    bool check(TreeNode* root, int *height){
+        if(root == NULL){
+            *height = 0;
+            return true;
+        }
+        int lh=0, rh=0;
+        bool left = check(root->left, &lh);
+        bool right = check(root->right, &rh);
+        *height = max(lh,rh) + 1;
+        if(abs(lh-rh)>1)
+            return false;
+        else
+            return left && right;
     }
 };
