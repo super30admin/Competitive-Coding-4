ca1fff397cb0691863781ccd95d6bb368f54b499
laxmisravyarudraraju <laxmisravyarudraraju@gmail.com>
2021-09-14 17:32:47 -0400

    Implemented Balanced Binary tree and Is Palindrome LL in java

---
+++ IsBalancedBT.java
@@ -0,0 +1,22 @@
+// TIME: O(n)
+// SPACE: O(h) where h = height of the tree
+
+public class IsBalancedBT {
+    private boolean balanced;
+    public boolean isBalanced(TreeNode root) {
+        balanced = true;
+        helper(root);
+        return balanced;
+    }
+    
+    private int helper(TreeNode node) {
+        if (node == null) return -1;
+        if (node.left == null && node.right == null) return 0;
+        int leftHeight = helper(node.left);
+        int rightHeight = helper(node.right);
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            balanced = false;
+        }
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
