1114a532396b7a8afe46b9369aa33563d5c00b99
Adhish Shrivastava <adhish70@gmail.com>
2021-11-02 09:40:15 -0400

    Initial Commit
    Initial commit

---
+++ Problem-58 Palindrome LL.py
@@ -0,0 +1,16 @@
+# 234. Palindrome Linked List
+# https://leetcode.com/problems/palindrome-linked-list/
+
+# Time Complexiety: 
+# Space Complexiety:
+
+# Logic: 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        pass
---
+++ Problem-59 Balanced BT.py
@@ -0,0 +1,17 @@
+# 110. Balanced Binary Tree
+# https://leetcode.com/problems/balanced-binary-tree/
+
+# Time Complexiety: 
+# Space Complexiety:
+
+# Logic: 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        pass
