03a2982528e9f3d33ca0b9de61135b7c04321520
Adhish Shrivastava <adhish70@gmail.com>
2021-11-02 17:15:10 -0400

    Problem59 done

--- Problem-59 Balanced BT.py
+++ Problem-59 Balanced BT.py
@@ -1,10 +1,8 @@
 # 110. Balanced Binary Tree
 # https://leetcode.com/problems/balanced-binary-tree/
 
-# Time Complexiety: 
-# Space Complexiety:
-
-# Logic: 
+# Time Complexiety: O(n)
+# Space Complexiety: O(n) [recurssion stack]
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -13,5 +11,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def _helper(self, root):
+        if root:
+            if root.left == None and root.right == None:
+                return 0
+
+            leftChild = self._helper(root.left) if root.left else -1
+            rightChild = self._helper(root.right) if root.right else -1
+            
+            if abs(leftChild-rightChild) > 1:
+                return -100000
+            
+            return max(leftChild, rightChild) + 1
+                
+    
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        pass
+        if not root:
+            return True
+        
+        result = self._helper(root)
+        
+        if result < 0:
+            return False
+        else:
+            return True
