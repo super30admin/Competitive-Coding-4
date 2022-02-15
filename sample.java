f6be32ee8dd32ebfe729a38bcce6405621eb5b31
gauravhjaisinghani <88104095+gauravhjaisinghani@users.noreply.github.com>
2022-02-15 01:12:22 -0500

    C-4 completed

--- sample.java
+++ sample.java
@@ -5,3 +5,86 @@
 
 
 // Your code here along with comments explaining your approach
+
+ // Definition for singly-linked list.
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        //o(n) time and O(1) space
+        ListNode slow = head;
+        ListNode fast = slow.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode temp = slow.next;
+        slow.next = null;
+
+        ListNode one = head;
+        ListNode two = reverse(temp);
+        
+        while(one != null && two!= null){
+            if(one.val != two.val) return false;
+            else {
+                one = one.next;
+                two = two.next;
+            }
+        }
+        return true;
+
+    }
+    private ListNode reverse(ListNode root){
+        if(root == null) return null;
+        ListNode curr = root;
+        ListNode prev = null;
+        ListNode fast = curr.next;
+        while(fast != null){
+            curr.next = prev;
+            prev = curr;
+            curr = fast;
+            fast = fast.next;
+        }
+        curr.next = prev;
+        return curr;
+    }
+}
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+
+        //o(n) time and const space
+        if(root == null ) return true;
+        return height(root) != -1;
+    }
+    private int height(TreeNode root){
+        if(root == null) return 0;
+        int left = height(root.left);
+        int right = height(root.right);
+        if(Math.abs(left - right) > 1 ) return -1;
+        if(left == -1 || right == -1) return -1;
+        return Math.max(left, right) +1;
+    }
+}
