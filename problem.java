90905f9e17bae980e179f7390cbd5bbe87e729f7
priyank <ppatel631@gmail.com>
2022-05-06 20:21:44 -0400

    Completed is palindrome

---
+++ problem.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+public class problem {
+    public boolean isPalindrome(ListNode head){
+        if(head == null) return head;
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode h = slow.next;
+        slow.next = null;
+        ListNode prev = null;
+        ListNode curr = h;
+        ListNode next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        h = prev;
+        while(head != null && h != null){
+            if(head.val != h.val ) return false;
+
+            head = head.next;
+            h = h.next;
+        }
+
+        return true;
+    }
+}
