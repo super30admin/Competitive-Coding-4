dad4d5ee923f0758bf4481feeedd495ee4408b74
jayachandravvs <42566670+jayachandravvs@users.noreply.github.com>
2022-05-24 23:26:47 -0500

    Update HeightBalancedTree.java

--- HeightBalancedTree.java
+++ HeightBalancedTree.java
@@ -1,1 +1,25 @@
+//o(nlogn) time || o(h) space
 
+class Solution {
+  // Recursively obtain the height of a tree. An empty tree has -1 height
+  private int height(TreeNode root) {
+    // An empty tree has height -1
+    if (root == null) {
+      return -1;
+    }
+    return 1 + Math.max(height(root.left), height(root.right));
+  }
+
+  public boolean isBalanced(TreeNode root) {
+    // An empty tree satisfies the definition of a balanced tree
+    if (root == null) {
+      return true;
+    }
+
+    // Check if subtrees have height within 1. If they do, check if the
+    // subtrees are balanced
+    return Math.abs(height(root.left) - height(root.right)) < 2
+        && isBalanced(root.left)
+        && isBalanced(root.right);
+  }
+};
