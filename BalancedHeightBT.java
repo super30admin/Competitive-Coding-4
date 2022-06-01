f9376dc642a98cfd4c20da6c4b23a0fc5a439b54
eshasah <esha8sah@gmail.com>
2022-06-01 16:43:28 -0700

    solution updated

--- BalancedHeightBT.java
+++ BalancedHeightBT.java
@@ -1,7 +1,7 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :O(nlogn)
+// Space Complexity :O(nlogn)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :No
 
 // Your code here along with comments explaining your approach
 class BalancedHeightBT {
@@ -20,6 +20,12 @@ class BalancedHeightBT {
     }
 }
 
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :No
+
+// Your code here along with comments explaining your approach
 class BalancedHeightSol2 {
     public boolean isBalanced(TreeNode root) {
         if (root == null)
