948fef571c47ef8d290ca716dd66d7bd848a855e
Darshil Shah <darshilshah1899@gmail.com>
2022-06-22 00:08:44 -0400

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,21 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
 
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+        int left_ht = get_ht(root.left);
+        int right_ht = get_ht(root.right);
+        if (Math.abs(left_ht - right_ht) > 1)
+            return false;
+        return (isBalanced(root.right) && isBalanced(root.left));
 
-// Your code here along with comments explaining your approach
+    }
+
+    public int get_ht(TreeNode root) {
+        if (root == null)
+            return 0;
+        int leftH = get_ht(root.left);
+        int rightH = get_ht(root.right);
+        return Math.max(leftH, rightH) + 1;
+    }
+}
