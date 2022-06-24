a68ae248a9072ad7daf98dce4b8b610321680793
Rajvi Shah <rajvishah2309@gmail.com>
2022-06-24 15:17:31 -0700

    completed

---
+++ Problem 1.py
@@ -0,0 +1,25 @@
+# time complexity = O(n) + O(n) = O(2n) = O(n)
+# space complexity = O(n) -> for copying n element of linked list to an array
+
+
+
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        vals = []
+        current_node = head
+        while current_node is not None:
+            vals.append(current_node.val)
+            current_node = current_node.next
+        return vals == vals[::-1]
+
+
+
+
