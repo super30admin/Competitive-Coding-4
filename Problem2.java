1511cce42f77bf38ff6610045e7649257880c67a
siddbanerjee2012 <sidd.banerjee2012@gmail.com>
2022-07-25 22:18:09 -0500

    Completed Sprint Competitive Coding-4

---
+++ Problem2.java
@@ -0,0 +1,45 @@
+// Time Complexity : O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null)
+            return true;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        fast = reverse(slow.next);
+        slow.next = null;
+        slow = head;
+        while(fast != null && slow != null){
+            if(fast.val != slow.val){
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode fast = head.next;
+        while(fast != null){
+            curr.next = prev;
+            prev = curr;
+            curr = fast;
+            fast = fast.next;
+        }
+        curr.next = prev;
+        return curr;
+    }
+}
