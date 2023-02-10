b6ce9f742c41177799d35b393a5af974e2cd6b32
Manthan Wadiwala <manthancar@gmail.com>
2023-02-09 23:39:46 -0500

    Completed CC-4

---
+++ BalancedBinaryTree.java
@@ -0,0 +1,46 @@
+// Approach 1: Calculate height for every node
+// Time Complexity : O(nlogn)
+// Space Complexity : O(n)
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null)
+            return true;
+        int left = height(root.left);
+        int right = height(root.right);
+        if(Math.abs(left-right)>1)
+            return false;
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+    private int height(TreeNode root){
+        if(root == null)
+            return 0;
+        return (1 + Math.max(height(root.left),height(root.right)));
+    }
+}
+
+// Approach 2: Bottom up approach
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null)
+            return true;
+        int height = height(root);
+        if(height==-1)
+            return false;
+        return true;
+    }
+    private int height(TreeNode root){
+        if(root == null)
+            return 0;
+        int left = height(root.left);
+        int right = height(root.right);
+        if(Math.abs(left-right)>1)
+            return -1;
+        if(left == -1 || right == -1)
+            return -1;
+        return 1 + Math.max(left,right);
+    }
+}
