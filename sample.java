7868ea688efba67ec92ea4b7875cc8e28e693fab
Apeksha Kotian <apekshakotian@Apekshas-MBP.attlocal.net>
2023-10-23 00:38:42 -0500

    Completed Competitive-Coding-4

--- sample.java
+++ sample.java
@@ -1,7 +1,82 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// https://leetcode.com/problems/palindrome-linked-list/
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : I didnt face any problem while coding this
 
 
 // Your code here along with comments explaining your approach
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null) return false;
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        fast = reverse(slow);
+        slow = head;
+        while(slow != null && fast != null){
+            if(slow.val != fast.val){
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head){
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
+
+}
+
+// https://leetcode.com/problems/balanced-binary-tree
+// Time Complexity : O(n log n)
+// Space Complexity : O(h)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : I didnt face any problem while coding this
+
+class Solution {
+  private int height(TreeNode root) {
+    
+    if (root == null) {
+      return -1;
+    }
+    return 1 + Math.max(height(root.left), height(root.right));
+  }
+
+  public boolean isBalanced(TreeNode root) {
+    
+    if (root == null) {
+      return true;
+    }
+
+    return Math.abs(height(root.left) - height(root.right)) < 2
+        && isBalanced(root.left)
+        && isBalanced(root.right);
+  }
+};
