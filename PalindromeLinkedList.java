81c31abce4bcebe2ad2740be49824bedecc9b988
pnayani75 <35510785+pnayani75@users.noreply.github.com>
2024-01-15 02:15:57 -0800

    Create PalindromeLinkedList.java

---
+++ PalindromeLinkedList.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// tc o(n)
+// sc o(1)
+class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null) return false;
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        fast = reverse(slow.next);
+        slow.next = null;
+        slow = head;
+        while(fast != null){
+            if(slow.val != fast.val){
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while(head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
