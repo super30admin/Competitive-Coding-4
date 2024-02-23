36411c7c1890540a499a1e767491620402d8faca
RohanKMeher <roha10meher@gmail.com>
2024-02-23 09:58:45 -0500

    corrects the Space Complexity of BalancedBinaryTree

--- BalancedBinaryTree.java
+++ BalancedBinaryTree.java
@@ -1,5 +1,5 @@
 // Time Complexity : O(n)
-// Space Complexity : O(1)
+// Space Complexity : O(h) [height of the binary tree]
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
