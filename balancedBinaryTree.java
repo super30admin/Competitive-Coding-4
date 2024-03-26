ae475c2a3da2c8173422c77ca4f73f3d60c0185c
Dhanashree Lale <dhanashreelale@Dhanashrees-MacBook-Pro.local>
2024-03-25 22:53:50 -0500

    completed CC 4

---
+++ balancedBinaryTree.java
@@ -0,0 +1,37 @@
+// Time Complexity : O(n)
+// Space Complexity : O(h) h--> height of the tree
+// Did this code successfully run on Leetcode : Yes
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+
+        if(root == null) return true;
+        return height(root) != -1;
+    }
+
+    private int height(TreeNode root){
+        //base
+        if(root == null) return 0;
+        int left = height(root.left);
+        int right = height(root.right);
+
+        if(Math.abs(left - right) > 1) return -1;
+        if(left == -1 || right == -1) return -1;
+        return Math.max(left,right) + 1;
+    }
+}
